68c2b2162fe2a2da7dcaf6e3f9fa39218cea6e39
Fouad Ouardi <86013594+fouadsphone@users.noreply.github.com>
2022-06-18 11:57:57 -0400

    Add files via upload

---
+++ Fouad-Ouardi_COMP212-002_Lab2/Fouad-Ouardi_Exercise01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fouad_Ouardi_Exercise01
+{
+    class Program
+    {
+        static Func<string, string, string, string> Minimum = (string1, string2, string3) =>
+        {
+            if (string.Compare(string1, string2, StringComparison.OrdinalIgnoreCase) < 0 && string.Compare(string1, string3, StringComparison.OrdinalIgnoreCase) < 0)
+            {
+                return string1;
+            }
+            else if (string.Compare(string2, string1, StringComparison.OrdinalIgnoreCase) < 0 && string.Compare(string2, string3, StringComparison.OrdinalIgnoreCase) < 0)
+            {
+                return string2;
+            }
+            else
+            {
+                return string3;
+            }
+        };
+
+        static Action<double, double, double> AvgGrade = (value1, value2, value3) =>
+        {
+            double avg = (value1 + value2 + value3) / 3;
+            Console.WriteLine("The average grade is " + avg + ":D");
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The smallest string is " + Minimum("Testing", "tested", "test") + " :D");
+            AvgGrade(78, 23, 65);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Fouad-Ouardi_COMP212-002_Lab2/Fouad-Ouardi_Exercise02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fouad_Ouardi_Exercise02
+{
+    class Program
+    {
+        public delegate bool GradesPredicate(double grade);
+        static void Main(string[] args)
+        {
+            int Min = 5;
+            int Max = 50;
+            double[] stGrade = new double[10];
+
+            Random rNum = new Random();
+            for (int i = 0; i < stGrade.Length; i++)
+            {
+                stGrade[i] = rNum.Next(Min, Max);
+            }
+            
+            GradesPredicate gradesPredicate = (grade) => { return grade >= 15.0; };
+
+            GradesFilter(stGrade, gradesPredicate);
+            Console.ReadLine();
+
+        }
+
+        public static void GradesFilter(double[] stGrade, GradesPredicate gradesPredicate)
+        {
+            var filteredStGrade = stGrade.Where(grade => gradesPredicate(grade));
+            foreach (var grade in filteredStGrade)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
